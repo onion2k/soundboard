dec1f79bcff123037d2ea924dbd1309ab9d0c35a
Chris <chris@usablehq.com>
2017-08-19 12:09:13 +0100

    Tile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Tile from './components/Tile/Tile';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Tile sound='sounds/canoe.ogg'></Tile>
       </div>
     );
   }
---
+++ src/components/Tile/Tile.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Sound from 'react-sound';
+import './Tile.css';
+
+import TiMediaPlay from 'react-icons/lib/ti/media-play';
+import TiMediaStop from 'react-icons/lib/ti/media-stop';
+
+function Play(props) {
+  const playing = props.playing;
+  if (playing===Sound.status.PLAYING) {
+    return <TiMediaStop className='playing' />;
+  }
+  return <TiMediaPlay />;
+}
+
+class Tile extends Component {
+  constructor(){
+    super();
+    this.state = { playing: Sound.status.STOPPED };
+    this.togglePlaying = this.togglePlaying.bind(this);
+    this.finished = this.finished.bind(this);
+  }
+  togglePlaying(e){
+      if (this.state.playing === Sound.status.STOPPED) {
+          this.setState({ playing: Sound.status.PLAYING });
+      } else {
+          this.setState({ playing: Sound.status.STOPPED });
+      }
+  }
+  finished(e){
+      this.setState({ playing: Sound.status.STOPPED })
+  }
+  render(props) {
+    return (
+      <div className="Tile" onClick={ this.togglePlaying }>
+        <Play playing={this.state.playing} />
+        <Sound url={this.props.sound} playStatus={this.state.playing} autoLoad={true} onFinishedPlaying={ this.finished } />
+      </div>
+    );
+  }
+}
+
+export default Tile;
