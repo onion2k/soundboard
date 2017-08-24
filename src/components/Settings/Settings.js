db038116689722da92c4336be0532ebe6bbe0f79
Chris Neale <chris@komododigital.co.uk>
2017-08-24 09:11:40 +0100

    Changed time signature

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -9,7 +9,7 @@ class Settings extends Component {
     render(props) {
         return (
             <div className={`Settings  ${this.state.open ? 'open' : ''}`} onClick={(e)=>{ this.setState({ open: !this.state.open }); }}>
-                <div className="title">Options</div>
+                <div className="title">Settings</div>
                 <div className="panel">
                     <div>Settings</div>
                 </div>
