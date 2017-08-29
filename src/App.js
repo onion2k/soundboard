9b24259f518d2f725333d0229d1eeddd3bbfc224
Chris Neale <chris@komododigital.co.uk>
2017-08-29 08:49:45 +0100

    Control added

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ class App extends Component {
             <div className="App">
                 <Header>New Retro Synthboard</Header>
                 <div className="Soundboard">{ this.board }</div>
+                <div>Song controls</div>
                 <Sequencer sounds={ this.state.sounds }></Sequencer>
                 <Settings></Settings>
             </div>
